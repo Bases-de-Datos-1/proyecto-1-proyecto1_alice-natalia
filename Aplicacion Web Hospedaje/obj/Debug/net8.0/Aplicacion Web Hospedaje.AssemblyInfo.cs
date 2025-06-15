0ea545074bc96c6d2f540ed2149addfa28f10cd9
Alice Arias Salazar <125696332+Alice-Arias@users.noreply.github.com>
2025-06-14 21:24:42 -0600

    aplicacion web
    inicio de la app

--- Aplicacion Web Hospedaje/obj/Debug/net8.0/Aplicacion Web Hospedaje.AssemblyInfo.cs
+++ Aplicacion Web Hospedaje/obj/Debug/net8.0/Aplicacion Web Hospedaje.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacion Web Hospedaje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4714a8a838ac78a516c664dfebeb6f9065b200")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb0ca2cb82eafa8f5ad6daa7e63c78999acabdd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacion Web Hospedaje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacion Web Hospedaje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
